4202cccf48ffc1125bac20cff77d30937543f7f8
Alex <collagememorycollage@gmail.com>
2026-07-27 20:08:54 +0300

    Добавил библиотеки для Virtual Box

--- install.sh
+++ install.sh
@@ -17,7 +17,15 @@ country_code=$(curl -s https://ipwho.is | grep "country_code" | cut -d'"' -f4)
 if [ $(id -u) == "0" ] && [ $country_code != "RU" ]; then	
 	#Update system
 	apt update -y && apt upgrade -y
-	
+
+	#Download package for Virtual Box
+	sudo apt install -y libvulkan1 libgl1i \
+	       	liblzf1 libpng16-16t64 libqt6core6t64 \
+	       	libqt6dbus6 libqt6gui6 libqt6help6 \
+	       	libqt6printsupport6 libqt6statemachine6 \
+	       	libqt6widgets6 libqt6xml6 libtpms0 libvpx9 libxt6t64
+
+		
 	#Download and install virtualbox
 	wget https://download.virtualbox.org/virtualbox/7.2.14/virtualbox-7.2_7.2.14-174565~Ubuntu~plucky_amd64.deb
 	dpkg -i virtualbox*
