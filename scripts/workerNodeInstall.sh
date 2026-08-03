fd9aecd2957c9682fb0dfbad636ad5fc48702875
Александр <107280620+collagememorycollage@users.noreply.github.com>
2026-08-03 20:41:25 +0000

    Update workerNodeInstall.sh

--- scripts/workerNodeInstall.sh
+++ scripts/workerNodeInstall.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+
+rke_token=$(cat /vagrant/rke2_token)
+
+
 curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sh -
 
 systemctl enable rke2-agent.service
 
 mkdir -p /etc/rancher/rke2/
-touch /etc/rancher/rke2/config.yaml
 
-echo """
+cat <<EOF > /etc/rancher/rke2/config.yaml
 server: https://192.168.41.101:9345
-token: <token from server node>
-""" > /etc/rancher/rke2/config.yaml 
+token: ${rke_token} 
+EOF
 
 systemctl start rke2-agent.service
 
